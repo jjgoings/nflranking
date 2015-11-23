a97b68c0c3d4ab481553776a5a028a19a2924aae
jjgoings <jjgoings@gmail.com>
2015-11-22 19:38:00 -0800

    Changed style to PEP8, added some docstrings and comments

--- ranking.py
+++ ranking.py
@@ -3,31 +3,41 @@ import sys
 import math
 import numpy as np
 
-def weight(method,t,t0,tf):
+
+def weight(method, t, t0, tf):
+    """ Evaluates a weight or scaling factor given a certain
+            method and time. For example, 'linear' weights older
+            games less than more recent games according to a
+            linear distribution.
+    """
     if method == None:
         return 1.0
     elif method == 'linear':
-        return 1.0 + np.true_divide(t-t0,tf-t0)
+        return 1.0 + np.true_divide(t - t0, tf - t0)
     elif method == 'log':
-        return 1.0 + np.log(1 + (np.true_divide(t-t0,tf-t0)))
+        return 1.0 + np.log(1 + (np.true_divide(t - t0, tf - t0)))
     elif method == 'exponential':
-        return np.exp(np.true_divide((t-t0),(tf-t0)))
+        return np.exp(np.true_divide((t - t0), (tf - t0)))
     elif method == 'biweekly':
-        return math.floor(((t-t0)/14)+1)
+        return math.floor(((t - t0) / 14) + 1)
     else:
         print " Not a valid weighting method, therefore continuing unweighted."
         return 1.0
 
 
-def massey(games,num_teams,team_dict,weight_method,criteria):
+def massey(games, num_teams, team_dict, weight_method, criteria):
+    """ Massey ranking method for teams given a set of games between
+           them and a weight method. Criteria ranks according to point
+           differential or win/loss or total yards differential.
+    """
     # Time info (start, end) for weighting
     t0 = int(games[0].eid[:8])
     tf = int(games[-1].eid[:8])
 
     # T contains diagonal info about total games played
-    T = np.zeros((num_teams,num_teams))
+    T = np.zeros((num_teams, num_teams))
     # P contains off-diagonal info about pairwise matchups
-    P = np.zeros((num_teams,num_teams))
+    P = np.zeros((num_teams, num_teams))
     # b gives total yard differentials
     b = np.zeros(num_teams)
     for game in games:
@@ -53,21 +63,25 @@ def massey(games,num_teams,team_dict,weight_method,criteria):
             w_yds = a_yds
             l_yds = h_yds
 
-        T[w,w] += 1.0*weight(weight_method,t,t0,tf)
-        T[l,l] += 1.0*weight(weight_method,t,t0,tf)
-        P[w,l] += 1.0*weight(weight_method,t,t0,tf)
-        P[l,w] += 1.0*weight(weight_method,t,t0,tf)
-        b[w] += (w_yds - l_yds)*weight(weight_method,t,t0,tf)
-        b[l] += (l_yds - w_yds)*weight(weight_method,t,t0,tf)
+        T[w, w] += 1.0 * weight(weight_method, t, t0, tf)
+        T[l, l] += 1.0 * weight(weight_method, t, t0, tf)
+        P[w, l] += 1.0 * weight(weight_method, t, t0, tf)
+        P[l, w] += 1.0 * weight(weight_method, t, t0, tf)
+        b[w] += (w_yds - l_yds) * weight(weight_method, t, t0, tf)
+        b[l] += (l_yds - w_yds) * weight(weight_method, t, t0, tf)
     Massey_Matrix = T - P
-    # Replace top row of Massey_Matrix with ones and corresponding 
+    # Replace top row of Massey_Matrix with ones and corresponding
     # entry in p_vector to zero to ensure full rank
-    Massey_Matrix[0,:] = np.ones(num_teams)
+    Massey_Matrix[0, :] = np.ones(num_teams)
     b[0] = 0.0
-    rating = np.linalg.solve(Massey_Matrix,b)
+    rating = np.linalg.solve(Massey_Matrix, b)
     return rating
 
-def colley(games,num_teams,team_dict,weight_method,criteria):
+
+def colley(games, num_teams, team_dict, weight_method, criteria):
+    """ Colley ranking method for teams given a set of games between
+           them and a weight method. Colley uses win/loss ONLY.
+    """
     if not criteria:
         print "Colley only uses win/loss, not points, or yards, etc"
 
@@ -75,10 +89,10 @@ def colley(games,num_teams,team_dict,weight_method,criteria):
     t0 = int(games[0].eid[:8])
     tf = int(games[-1].eid[:8])
 
-    Colley_Matrix = np.zeros((num_teams,num_teams))
+    Colley_Matrix = np.zeros((num_teams, num_teams))
 
     # Add two to the diagonal
-    Colley_Matrix += 2*np.eye(num_teams)
+    Colley_Matrix += 2 * np.eye(num_teams)
     b_vector = np.ones(num_teams)
     for game in games:
         # grab time of game, set to 't'
@@ -96,15 +110,16 @@ def colley(games,num_teams,team_dict,weight_method,criteria):
         # need to determine how to handle ties!
 
         # Add a game played to team's diagonal element
-        Colley_Matrix[w,w] += 1.0*weight(weight_method,t,t0,tf)
-        Colley_Matrix[l,l] += 1.0*weight(weight_method,t,t0,tf)
+        Colley_Matrix[w, w] += 1.0 * weight(weight_method, t, t0, tf)
+        Colley_Matrix[l, l] += 1.0 * weight(weight_method, t, t0, tf)
         # Add negative one to team's off diagonal
-        Colley_Matrix[w,l] += -1.0*weight(weight_method,t,t0,tf)
-        Colley_Matrix[l,w] += -1.0*weight(weight_method,t,t0,tf)
-        b_vector[w] += 0.5*weight(weight_method,t,t0,tf)
-        b_vector[l] += -0.5*weight(weight_method,t,t0,tf)
-    rating = np.linalg.solve(Colley_Matrix,b_vector)
+        Colley_Matrix[w, l] += -1.0 * weight(weight_method, t, t0, tf)
+        Colley_Matrix[l, w] += -1.0 * weight(weight_method, t, t0, tf)
+        b_vector[w] += 0.5 * weight(weight_method, t, t0, tf)
+        b_vector[l] += -0.5 * weight(weight_method, t, t0, tf)
+    rating = np.linalg.solve(Colley_Matrix, b_vector)
     return rating
+
 
 def main():
     """Main entry point for the script."""
--- season.py
+++ season.py
@@ -3,28 +3,30 @@ import sys
 import nflgame
 import ranking
 
+
 class Season(object):
 
     def update_games(self):
-        return nflgame.games(self._year,      \
-                             week=self._week, \
+        """ Update 'games' given new 'games' properties """
+        return nflgame.games(self._year,
+                             week=self._week,
                              kind=self._kind)
 
     def __init__(self):
-        self._year      = 2015
-        self._week      = None
-        self._kind      = 'REG'
-        self._teams     = nflgame.teams
-        self._games     = self.update_games()
+        self._year = 2015   # Any year 2009 to present.
+        self._week = None   # Specify individual weeks as integer.
+        self._kind = 'REG'  # kind = 'REG', 'POST', 'PRE' for reg. season, etc.
+        self._teams = nflgame.teams
+        self._games = self.update_games()
         self._num_teams = len(self._teams)
         self._num_games = len(self._games)
-        # map team names to integer, and vice versa
+        # _team_dict maps team names to integer, and vice versa
         self._team_dict = {}
         for idx in xrange(self._num_teams):
             self._team_dict[idx] = str(self._teams[idx][0])
             self._team_dict[str(self._teams[idx][0])] = idx
         self._rating_vector = None
-        self._rating        = None
+        self._rating = None
 
     @property
     def games(self):
@@ -33,30 +35,33 @@ class Season(object):
     @property
     def year(self):
         return self._year
+
     @year.setter
-    def year(self,value):
+    def year(self, value):
         if self._year != value:
-            self._year  = value
+            self._year = value
             self._games = self.update_games()
             self._num_games = len(self._games)
 
     @property
     def week(self):
         return self._week
+
     @week.setter
-    def week(self,value):
+    def week(self, value):
         if self._week != value:
-            self._week  = value
+            self._week = value
             self._games = self.update_games()
             self._num_games = len(self._games)
 
     @property
     def kind(self):
         return self._kind
+
     @kind.setter
-    def kind(self,value):
+    def kind(self, value):
         if self._kind != value:
-            self._kind  = value
+            self._kind = value
             self._games = self.update_games()
             self._num_games = len(self._games)
 
@@ -72,31 +77,32 @@ class Season(object):
     def rating(self):
         result = []
         for i in xrange(self._num_teams):
-            result.append([self._team_dict[i],self._rating_vector[i]])
-        self._rating = sorted(result,key=lambda x: x[1],reverse=True)
+            result.append([self._team_dict[i], self._rating_vector[i]])
+        self._rating = sorted(result, key=lambda x: x[1], reverse=True)
         return self._rating
 
-
-    def massey(self,weight=None,criteria=None):
-        self._rating_vector = ranking.massey(self._games, \
-                                             self._num_teams, \
-                                             self._team_dict, \
-                                             weight, \
+    def massey(self, weight=None, criteria=None):
+        """ Wrapper for Massey rating method """
+        self._rating_vector = ranking.massey(self._games,
+                                             self._num_teams,
+                                             self._team_dict,
+                                             weight,
                                              criteria)
 
-    def colley(self,weight=None,criteria=None):
+    def colley(self, weight=None, criteria=None):
+        """ Wrapper for Colley rating method """
         # note criteria is useless for colley --- always uses win/loss data
-        self._rating_vector = ranking.massey(self._games, \
-                                             self._num_teams, \
-                                             self._team_dict, \
-                                             weight, \
+        self._rating_vector = ranking.massey(self._games,
+                                             self._num_teams,
+                                             self._team_dict,
+                                             weight,
                                              criteria)
 
 
 def main():
-    """Main entry point for the script."""
+    """Main entry. Prints ranked teams in order."""
     season = Season()
-    season.massey('log','total_yards')
+    season.massey('log', 'total_yards')
     for team in xrange(season.num_teams):
         print season.rating[team]
 
